8476ac2eee222c1c2c5887c7359983c39b2de4a9
unknown <vannchhit0709@gmail.com>
2024-08-26 16:36:50 +0700

    feature signub working

--- devsphere-develop/src/layouts/site/Header.tsx
+++ devsphere-develop/src/layouts/site/Header.tsx
@@ -1,14 +1,18 @@
+import React from "react";
 import csxLogo from "@src/assets/logos/csx_logo.gif";
 import LanguageToggle from "@src/components/LanguageSwitcher";
 import { MegaMenu } from "primereact/megamenu";
 import LngLink from "@src/components/LngLink";
 import { MenuItem } from "primereact/menuitem";
 import useLngNavigate from "@src/hooks/useLngNavigate";
+import "./style.css";
 
 export default function Header() {
   const navigate = useLngNavigate();
+
   const items: MenuItem[] = [
     {
+      /*
       label: "About",
       icon: "pi pi-box",
       items: [
@@ -26,9 +30,94 @@ export default function Header() {
           },
         ],
       ],
+    */
+    },
+    {
+      label: "Business",
+      className: "menu-parent-label",
+      items: [
+        { 
+          items: [
+            {
+              label: "Business Overview",
+            },
+            {
+              label: "Capital Market IT Service",
+            },
+            {
+              label: "IT Infrastructure Service",       
+            },
+            {
+              label: "Financial Information Service",
+            },
+            {
+              label: "Blockchain Service",
+            },
+            {
+              label: "New Business",
+            }
+          ]  
+        },
+      ],
+    },
+    {
+      label: "Media Room",
+      className: "menu-parent-label",
+      items: [
+        { 
+          items: [
+            {
+              label: "CI Introduction",
+            },
+            {
+              label: "Brochure",
+            },
+            {
+              label: "PR Movie Clip",       
+            },
+            {
+              label: "At a Glance",
+            }
+          ]  
+        },
+      ],
+    },
+    {
+      label: "About Us",
+      className: "menu-parent-label",
+      items: [
+        { 
+          items: [
+            {
+              label: "Company Information",
+            },
+            {
+              label: "Organization Chart",
+            },
+            {
+              label: "Sitemap",       
+            },
+          ]  
+        },
+      ],
+    },
+    {
+      label: "Contact",
+      className: "menu-parent-label",
+      items: [
+        {
+          items: [
+            {
+              label: "Customer Center",
+            },
+            {
+              label: "Location & Contact",
+            },
+          ],
+        },
+      ],
     },
   ];
-
   const start = (
     <LngLink to={"/"}>
       <img
@@ -36,7 +125,7 @@ export default function Header() {
         className="mr-2"
         style={{ minWidth: "70px", height: "40px" }}
         src={csxLogo}
-      ></img>
+      />
     </LngLink>
   );
 
@@ -48,13 +137,13 @@ export default function Header() {
 
   return (
     <div className="sticky top-0 shadow-md bg-white z-50">
-      <div className="max-w-screen-2xl mx-auto">
+      <div className="w-full ">
         <MegaMenu
           model={items}
-          className="border-none bg-white text-base lg:text-sm"
+          className="border-none bg-white text-base lg:text-sm text-sm"
           start={start}
           end={end}
-          breakpoint="1024px"
+          breakpoint="1200px" // Adjust this value as needed
         />
       </div>
     </div>
--- devsphere-develop/src/pages/site/contact.tsx
+++ devsphere-develop/src/pages/site/contact.tsx
@@ -1,5 +1,5 @@
 const Contact = () => {
-  return <div>Contact</div>;
+  return <div>Contact info</div>;
 };
 
 export default Contact;
